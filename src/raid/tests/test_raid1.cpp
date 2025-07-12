9eb7cc1426dfe957c3402a64dd7f564c5bfdbceb
Brian Szmyd <bszmyd@ebay.com>
2025-07-12 17:47:40 +0000

    raid1: New immediate fail/sb update fail test case.

--- src/raid/tests/test_raid1.cpp
+++ src/raid/tests/test_raid1.cpp
@@ -689,6 +689,55 @@ TEST(Raid1, WriteFailImmediate) {
     EXPECT_TO_WRITE_SB(device_b);
 }
 
+// Immediate Write Fail
+TEST(Raid1, WriteFailImmediateFailSBupdate) {
+    auto device_a = CREATE_DISK(TestParams{.capacity = Gi});
+    auto device_b = CREATE_DISK(TestParams{.capacity = Gi});
+    auto raid_device = ublkpp::Raid1Disk(boost::uuids::random_generator()(), device_a, device_b);
+
+    {
+        EXPECT_CALL(*device_a, async_iov(_, _, _, _, _, _))
+            .Times(1)
+            .WillOnce(
+                [](ublksrv_queue const*, ublk_io_data const*, ublkpp::sub_cmd_t, iovec*, uint32_t, uint64_t const) {
+                    return folly::makeUnexpected(std::make_error_condition(std::errc::io_error));
+                });
+        EXPECT_TO_WRITE_SB_F(device_b, true); // Fail the attempt to dirty the SB
+
+        auto ublk_data = make_io_data(0xcafedead, UBLK_IO_OP_WRITE);
+        auto res = raid_device.handle_rw(nullptr, &ublk_data, 0b10, nullptr, 4 * Ki, 8 * Ki);
+        remove_io_data(ublk_data);
+        ASSERT_FALSE(res);
+    }
+
+    // Subsequent writes should continue to try device A this time succeeding dirty
+    auto ublk_data = make_io_data(0xcafedeae, UBLK_IO_OP_WRITE);
+    EXPECT_TO_WRITE_SB(device_b);
+    EXPECT_CALL(*device_a, async_iov(_, _, _, _, _, _))
+            .WillOnce(
+                [](ublksrv_queue const*, ublk_io_data const*, ublkpp::sub_cmd_t, iovec*, uint32_t, uint64_t const) {
+                    return folly::makeUnexpected(std::make_error_condition(std::errc::io_error));
+                });
+    EXPECT_CALL(*device_b, async_iov(_, _, _, _, _, _))
+        .Times(1)
+        .WillOnce([](ublksrv_queue const*, ublk_io_data const* data, ublkpp::sub_cmd_t sub_cmd, iovec* iovecs, uint32_t,
+                     uint64_t addr) {
+            EXPECT_EQ(data->tag, 0xcafedeae);
+            EXPECT_EQ(sub_cmd & ublkpp::_route_mask, 0b101);
+            EXPECT_FALSE(test_flags(sub_cmd, ublkpp::sub_cmd_flags::REPLICATED));
+            EXPECT_EQ(iovecs->iov_len, 4 * Ki);
+            EXPECT_EQ(addr, (8 * Ki) + reserved_size);
+            return 1;
+        });
+    auto res = raid_device.handle_rw(nullptr, &ublk_data, 0b10, nullptr, 4 * Ki, 8 * Ki);
+    remove_io_data(ublk_data);
+    ASSERT_TRUE(res);
+    EXPECT_EQ(1, res.value());
+
+    // expect unmount_clean on Device B
+    EXPECT_TO_WRITE_SB(device_b);
+}
+
 // Failed Discards should flip the route too
 TEST(Raid1, Discard) {
     auto device_a = CREATE_DISK(TestParams{.capacity = Gi});
