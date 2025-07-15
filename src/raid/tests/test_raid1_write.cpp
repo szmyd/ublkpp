20c632c613cde4f7476539aa7bef4fb0dbebee23
Brian Szmyd <bszmyd@ebay.com>
2025-07-15 21:10:02 +0000

    Test coverage

--- src/raid/tests/test_raid1_write.cpp
+++ src/raid/tests/test_raid1_write.cpp
@@ -230,7 +230,6 @@ TEST(Raid1, WriteDoubleFailure) {
 
     // Follow up retry on device A fails without operations on B
     {
-        RLOGI("Here");
         EXPECT_CALL(*device_b, async_iov(_, _, _, _, _, _)).Times(0);
 
         auto ublk_data = make_io_data(UBLK_IO_OP_WRITE);
@@ -240,6 +239,38 @@ TEST(Raid1, WriteDoubleFailure) {
         ASSERT_FALSE(res);
     }
 
+    // Follow up on device A fails without operations on B
+    {
+        EXPECT_CALL(*device_b, async_iov(_, _, _, _, _, _)).Times(0);
+        EXPECT_CALL(*device_a, async_iov(_, _, _, _, _, _))
+            .Times(1)
+            .WillOnce([](ublksrv_queue const*, ublk_io_data const*, ublkpp::sub_cmd_t, iovec*, uint32_t, uint64_t) {
+                return folly::makeUnexpected(std::make_error_condition(std::errc::io_error));
+            });
+
+        auto ublk_data = make_io_data(UBLK_IO_OP_WRITE);
+        auto res = raid_device.handle_rw(nullptr, &ublk_data, 0b10, nullptr, 12 * Ki, 16 * Ki);
+        remove_io_data(ublk_data);
+        ASSERT_FALSE(res);
+    }
+
+    // Failure to dirty pages
+    {
+        EXPECT_CALL(*device_b, async_iov(_, _, _, _, _, _)).Times(0);
+        EXPECT_CALL(*device_a, async_iov(_, _, _, _, _, _))
+            .Times(2)
+            .WillOnce([](ublksrv_queue const*, ublk_io_data const*, ublkpp::sub_cmd_t, iovec*, uint32_t, uint64_t) {
+                return 1;
+            })
+            .WillOnce([](ublksrv_queue const*, ublk_io_data const*, ublkpp::sub_cmd_t, iovec*, uint32_t, uint64_t) {
+                return folly::makeUnexpected(std::make_error_condition(std::errc::io_error));
+            });
+
+        auto ublk_data = make_io_data(UBLK_IO_OP_WRITE);
+        auto res = raid_device.handle_rw(nullptr, &ublk_data, 0b10, nullptr, 12 * Ki, 256 * Ki);
+        remove_io_data(ublk_data);
+        ASSERT_FALSE(res);
+    }
     // expect unmount_clean on last working device
     EXPECT_TO_WRITE_SB_F(device_a, true);
 }
