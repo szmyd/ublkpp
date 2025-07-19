d42412acfcfccdc81dd3c496c3f0ba59643451b7
Brian Szmyd <bszmyd@ebay.com>
2025-07-19 03:35:35 +0000

    raid1: test load_bitmap

--- src/raid/raid1/tests/bitmap/load_bitmap.cpp
+++ src/raid/raid1/tests/bitmap/load_bitmap.cpp
@@ -1,28 +1,26 @@
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
-#include <isa-l/mem_routines.h>
-
 #include "tests/test_disk.hpp"
 #include "raid/raid1/bitmap.hpp"
 
 using ::testing::_;
 
-// Ensure that all required pages are initialized
-TEST(Raid1, InitBitmap) {
+// Ensure that all required pages are read
+TEST(Raid1, LoadBitmap) {
     auto device = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = 2 * ublkpp::Gi});
-    auto bitmap = ublkpp::raid1::Bitmap(2 * ublkpp::Gi, 32 * ublkpp::Ki, 4 * ublkpp::Ki);
+    auto bitmap = ublkpp::raid1::Bitmap(3 * ublkpp::Gi, 32 * ublkpp::Ki, 4 * ublkpp::Ki);
 
-    EXPECT_CALL(*device, sync_iov(UBLK_IO_OP_WRITE, _, _, _))
-        .Times(2)
+    EXPECT_CALL(*device, sync_iov(UBLK_IO_OP_READ, _, _, _))
+        .Times(3)
         .WillRepeatedly([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> ublkpp::io_result {
             EXPECT_EQ(1U, nr_vecs);
             EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
             EXPECT_GE(addr, ublkpp::raid1::k_page_size);   // Expect write to bitmap!
             EXPECT_LT(addr, ublkpp::raid1::reserved_size); // Expect write to bitmap!
-            EXPECT_EQ(0, isal_zero_detect(iovecs->iov_base, ublkpp::raid1::k_page_size));
+            memset(iovecs->iov_base, 000, iovecs->iov_len);
             return ublkpp::raid1::k_page_size;
         });
 
-    bitmap.init_to(*device);
+    bitmap.load_from(*device);
 }
