570c95e695de228f462066fe4dd0c8ac51b325ac
Brian Szmyd <brian.szmyd@protonmail.com>
2025-07-21 09:47:06 -0600

    raid1: Test wrong UUID in SB (#64)

---
+++ src/raid/raid1/tests/superblock/wrong_uuid.cpp
@@ -0,0 +1,56 @@
+#include "test_raid1_common.hpp"
+
+#include <isa-l/mem_routines.h>
+
+// If we initialize with one clean device and one with the wrong uuid in the superblock
+TEST(Raid1, WrongUUIDA) {
+    auto device_a = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
+    auto device_b = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
+
+    EXPECT_CALL(*device_a, sync_iov(UBLK_IO_OP_READ, _, _, _))
+        .Times(1)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0UL, addr);
+            memset(iovecs->iov_base, 000, iovecs->iov_len);
+            memcpy(iovecs->iov_base, raid1_header, sizeof(ublkpp::raid1::SuperBlock::header));
+            auto superblock = reinterpret_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base);
+            superblock->header.uuid[0] = 0xbb;
+            return ublkpp::raid1::k_page_size;
+        });
+
+    EXPECT_THROW(ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b), std::runtime_error);
+}
+
+TEST(Raid1, WrongUUIDB) {
+    auto device_a = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
+    auto device_b = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
+
+    EXPECT_CALL(*device_a, sync_iov(UBLK_IO_OP_READ, _, _, _))
+        .Times(1)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0UL, addr);
+            memset(iovecs->iov_base, 000, iovecs->iov_len);
+            memcpy(iovecs->iov_base, raid1_header, sizeof(ublkpp::raid1::SuperBlock::header));
+
+            return ublkpp::raid1::k_page_size;
+        });
+
+    EXPECT_CALL(*device_b, sync_iov(UBLK_IO_OP_READ, _, _, _))
+        .Times(1)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0UL, addr);
+            memset(iovecs->iov_base, 000, iovecs->iov_len);
+            memcpy(iovecs->iov_base, raid1_header, sizeof(ublkpp::raid1::SuperBlock::header));
+            auto superblock = reinterpret_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base);
+            superblock->header.uuid[0] = 0xbb;
+            return ublkpp::raid1::k_page_size;
+        });
+
+    EXPECT_THROW(ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b), std::runtime_error);
+}
