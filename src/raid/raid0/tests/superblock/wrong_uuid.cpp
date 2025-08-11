52fcd3c4d576cf8ff3bb70ae20fe34b8547560b7
Brian Szmyd <bszmyd@ebay.com>
2025-08-11 15:52:34 +0000

    Fix leak for invalid raid0 superblock

--- src/raid/raid0/tests/superblock/wrong_uuid.cpp
+++ src/raid/raid0/tests/superblock/wrong_uuid.cpp
@@ -1,7 +1,7 @@
-#include "test_raid1_common.hpp"
+#include "test_raid0_common.hpp"
 
 // If we initialize with one clean device and one with the wrong uuid in the superblock
-TEST(Raid1, WrongUUIDA) {
+TEST(Raid0, WrongUUIDA) {
     auto device_a = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
     auto device_b = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
 
@@ -9,19 +9,20 @@ TEST(Raid1, WrongUUIDA) {
         .Times(1)
         .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
             EXPECT_EQ(1U, nr_vecs);
-            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(ublkpp::raid0::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
             EXPECT_EQ(0UL, addr);
-            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid1::k_page_size);
-            auto superblock = reinterpret_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base);
+            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid0::k_page_size);
+            auto superblock = reinterpret_cast< ublkpp::raid0::SuperBlock* >(iovecs->iov_base);
             superblock->header.uuid[0] = 0xbb;
-            return ublkpp::raid1::k_page_size;
+            return ublkpp::raid0::k_page_size;
         });
 
-    EXPECT_THROW(ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b),
+    EXPECT_THROW(ublkpp::Raid0Disk(boost::uuids::string_generator()(test_uuid), 128 * Ki,
+                                   std::vector< std::shared_ptr< UblkDisk > >{device_a, device_b}),
                  std::runtime_error);
 }
 
-TEST(Raid1, WrongUUIDB) {
+TEST(Raid0, WrongUUIDB) {
     auto device_a = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
     auto device_b = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi});
 
@@ -29,25 +30,26 @@ TEST(Raid1, WrongUUIDB) {
         .Times(1)
         .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
             EXPECT_EQ(1U, nr_vecs);
-            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(ublkpp::raid0::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
             EXPECT_EQ(0UL, addr);
-            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid1::k_page_size);
+            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid0::k_page_size);
 
-            return ublkpp::raid1::k_page_size;
+            return ublkpp::raid0::k_page_size;
         });
 
     EXPECT_CALL(*device_b, sync_iov(UBLK_IO_OP_READ, _, _, _))
         .Times(1)
         .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
             EXPECT_EQ(1U, nr_vecs);
-            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(ublkpp::raid0::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
             EXPECT_EQ(0UL, addr);
-            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid1::k_page_size);
-            auto superblock = reinterpret_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base);
+            memcpy(iovecs->iov_base, &normal_superblock, ublkpp::raid0::k_page_size);
+            auto superblock = reinterpret_cast< ublkpp::raid0::SuperBlock* >(iovecs->iov_base);
             superblock->header.uuid[0] = 0xbb;
-            return ublkpp::raid1::k_page_size;
+            return ublkpp::raid0::k_page_size;
         });
 
-    EXPECT_THROW(ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b),
+    EXPECT_THROW(ublkpp::Raid0Disk(boost::uuids::string_generator()(test_uuid), 128 * Ki,
+                                   std::vector< std::shared_ptr< UblkDisk > >{device_a, device_b}),
                  std::runtime_error);
 }
--- src/raid/raid0/tests/test_raid0_common.hpp
+++ src/raid/raid0/tests/test_raid0_common.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/string_generator.hpp>
+
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
 #include <ublksrv.h>
@@ -15,6 +17,21 @@ using ::testing::Return;
 using ::ublkpp::Gi;
 using ::ublkpp::io_result;
 using ::ublkpp::UblkDisk;
+
+// This RAID0 header is copied to simulate loading a previous clean device
+static const ublkpp::raid0::SuperBlock normal_superblock =
+    {.header = {.magic = {0127, 0345, 072, 0211, 0254, 033, 070, 0146, 0125, 0377, 0204, 065, 0131, 0120, 0306, 047},
+                .version = htobe16(1),
+                .uuid = {0xad, 0xa4, 0x07, 0x37, 0x30, 0xe3, 0x49, 0xfe, 0x99, 0x42, 0x5a, 0x28, 0x7d, 0x71, 0xeb,
+                         0x3f}},
+     .fields =
+         {
+             .stripe_off = 0,
+             .stripe_size = htobe32(128 * Ki),
+         },
+     ._reserved = {0x00}};
+
+static std::string const test_uuid("ada40737-30e3-49fe-9942-5a287d71eb3f");
 
 #define EXPECT_SYNC_OP(OP, device, fail, sz, off)                                                                      \
     EXPECT_CALL(*(device), sync_iov(OP, _, _, _))                                                                      \
