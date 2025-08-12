53e4f252c347dc64aedfcc2b62f8d2523fac567f
Brian Szmyd <bszmyd@ebay.com>
2025-08-12 23:08:25 +0000

    swap coverage

--- src/raid/raid1/tests/misc/swap_device.cpp
+++ src/raid/raid1/tests/misc/swap_device.cpp
@@ -3,7 +3,7 @@
 #include <isa-l/mem_routines.h>
 
 /// Test the ability to swap one device for another
-TEST(Raid1, SwapDevice) {
+TEST(Raid1, SwapDeviceB) {
     auto device_a = CREATE_DISK_A((TestParams{.capacity = Gi, .id = "DiskA"}));
     auto device_b = CREATE_DISK_B((TestParams{.capacity = Gi, .id = "DiskB"}));
 
@@ -30,6 +30,75 @@ TEST(Raid1, SwapDevice) {
             return ublkpp::raid1::k_page_size;
         });
 
+    auto new_device = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi, .id = "DiskC"});
+    EXPECT_CALL(*new_device, sync_iov(UBLK_IO_OP_READ, _, _, _))
+        .Times(1)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0UL, addr);
+            memset(iovecs->iov_base, 000, iovecs->iov_len);
+            return ublkpp::raid1::k_page_size;
+        });
+    EXPECT_CALL(*new_device, sync_iov(UBLK_IO_OP_WRITE, _, _, _))
+        .Times(2)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_GE(addr, ublkpp::raid1::k_page_size);                                  // Expect write to bitmap!
+            EXPECT_LT(addr, ublkpp::raid1::reserved_size);                                // Expect write to bitmap!
+            EXPECT_EQ(0, isal_zero_detect(iovecs->iov_base, ublkpp::raid1::k_page_size)); // All zeros
+            return ublkpp::raid1::k_page_size;
+        })
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0UL, addr);
+            EXPECT_EQ(0, memcmp(&normal_superblock, iovecs->iov_base, sizeof(ublkpp::raid1::SuperBlock::header)));
+            EXPECT_EQ(1, static_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base)->fields.device_b);
+            return ublkpp::raid1::k_page_size;
+        });
+
+    auto old_device = raid_device.swap_device("DiskB", new_device);
+    EXPECT_EQ(old_device, device_b);
+
+    old_device = raid_device.swap_device("DiskA", device_b);
+    EXPECT_EQ(old_device, device_b);
+
+    // expect unmount_clean update
+    EXPECT_TO_WRITE_SB(device_a);
+}
+
+TEST(Raid1, SwapDeviceA) {
+    auto device_a = CREATE_DISK_A((TestParams{.capacity = Gi, .id = "DiskA"}));
+    auto device_b = CREATE_DISK_B((TestParams{.capacity = Gi, .id = "DiskB"}));
+
+    auto raid_device = ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b);
+
+    EXPECT_CALL(*device_b, sync_iov(UBLK_IO_OP_WRITE, _, _, _))
+        .Times(2)
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_EQ(0, memcmp(&normal_superblock, iovecs->iov_base, sizeof(ublkpp::raid1::SuperBlock::header)));
+            auto superblock = reinterpret_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base);
+            EXPECT_EQ(ublkpp::raid1::read_route::DEVB,
+                      static_cast< ublkpp::raid1::read_route >(superblock->fields.read_route));
+            EXPECT_EQ(0UL, addr);
+            return ublkpp::raid1::k_page_size;
+        })
+        .WillOnce([](uint8_t, iovec* iovecs, uint32_t nr_vecs, off_t addr) -> io_result {
+            EXPECT_EQ(1U, nr_vecs);
+            EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
+            EXPECT_GE(addr, ublkpp::raid1::k_page_size);                                  // Expect write to bitmap!
+            EXPECT_LT(addr, ublkpp::raid1::reserved_size);                                // Expect write to bitmap!
+            EXPECT_NE(0, isal_zero_detect(iovecs->iov_base, ublkpp::raid1::k_page_size)); // All ones
+            return ublkpp::raid1::k_page_size;
+        });
+
+    auto small_device = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Mi, .id = "DiskD"});
+    auto old_device = raid_device.swap_device("DiskA", small_device);
+    EXPECT_EQ(old_device, small_device);
 
     auto new_device = std::make_shared< ublkpp::TestDisk >(TestParams{.capacity = Gi, .id = "DiskC"});
     EXPECT_CALL(*new_device, sync_iov(UBLK_IO_OP_READ, _, _, _))
@@ -56,12 +125,13 @@ TEST(Raid1, SwapDevice) {
             EXPECT_EQ(ublkpp::raid1::k_page_size, ublkpp::__iovec_len(iovecs, iovecs + nr_vecs));
             EXPECT_EQ(0UL, addr);
             EXPECT_EQ(0, memcmp(&normal_superblock, iovecs->iov_base, sizeof(ublkpp::raid1::SuperBlock::header)));
+            EXPECT_EQ(0, static_cast< ublkpp::raid1::SuperBlock* >(iovecs->iov_base)->fields.device_b);
             return ublkpp::raid1::k_page_size;
         });
 
-    auto old_device = raid_device.swap_device("DiskB", new_device);
-    EXPECT_EQ(old_device, device_b);
+    old_device = raid_device.swap_device("DiskA", new_device);
+    EXPECT_EQ(old_device, device_a);
 
     // expect unmount_clean update
-    EXPECT_TO_WRITE_SB(device_a);
+    EXPECT_TO_WRITE_SB(device_b);
 }
