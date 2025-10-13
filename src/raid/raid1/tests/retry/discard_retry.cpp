57db9f11611cbd8eec9645f222567fa8f06e71a0
Brian Szmyd <bszmyd@ebay.com>
2025-10-13 09:28:44 -0700

    Fix buggy ut.

--- src/raid/raid1/tests/retry/discard_retry.cpp
+++ src/raid/raid1/tests/retry/discard_retry.cpp
@@ -5,6 +5,7 @@ TEST(Raid1, DiscardRetry) {
     auto device_a = CREATE_DISK_A(TestParams{.capacity = Gi});
     auto device_b = CREATE_DISK_B(TestParams{.capacity = Gi});
     auto raid_device = ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b);
+    raid_device.toggle_resync(false);
 
     {
         EXPECT_TO_WRITE_SB(device_a);
