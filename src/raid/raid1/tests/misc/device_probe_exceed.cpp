5b50bda35092ac9cd6768910e242045d56a54849
Illia Ilin <fate98765@gmail.com>
2026-02-05 01:01:26 +0000

    change supported capacity

--- src/raid/raid1/tests/misc/device_probe_exceed.cpp
+++ src/raid/raid1/tests/misc/device_probe_exceed.cpp
@@ -1,13 +1,20 @@
 #include "test_raid1_common.hpp"
 
 // Brief: Test that RAID1 array maintains a self-imposing limit to restrict the reserved size
+// The SuperBitmap can track at most 32,176 bitmap pages (4022 bytes * 8 bits/byte)
+// With 32KiB chunks, each bitmap page covers 1 GiB, giving a max capacity of ~31.4 TiB
 //
 TEST(Raid1, DevicesLargerThanAllowed) {
     auto device_a = CREATE_DISK_A(TestParams{.capacity = UINT64_MAX});
     auto device_b = CREATE_DISK_B(TestParams{.capacity = UINT64_MAX});
 
     auto raid_device = ublkpp::Raid1Disk(boost::uuids::string_generator()(test_uuid), device_a, device_b);
-    EXPECT_EQ(raid_device.capacity(), 0xFFFF80000FF80000);
+
+    // SuperBitmap max: 32,176 pages * 1 GiB/page = 31.42 TiB
+    // After subtracting reserved space for SuperBlock + bitmap pages, user capacity is slightly less
+    constexpr auto expected_max_capacity = 31ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL;  // ~31 TiB
+    EXPECT_GT(raid_device.capacity(), expected_max_capacity);  // Should be just over 31 TiB
+    EXPECT_LT(raid_device.capacity(), 32ULL * 1024ULL * 1024ULL * 1024ULL * 1024ULL);  // Should be less than 32 TiB
     EXPECT_STREQ(raid_device.id().c_str(), "RAID1");
 
     // CanDiscard and DirectIO `true` be default.
